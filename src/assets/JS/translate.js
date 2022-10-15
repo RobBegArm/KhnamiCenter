36721333967a84cf1f99db0ae64f3fdfb340ad38
Robert Beglaryan <robbegarm@gmail.com>
2022-10-15 15:28:50 +0300

    Update

--- src/assets/JS/translate.js
+++ src/assets/JS/translate.js
@@ -7,7 +7,7 @@ const armenianLanguage = Array.from(
   document.querySelectorAll(".language--arm")
 );
 
-//Selects the title element, and set an active title
+// Selects the title element, and set an active title
 const title = document.querySelector("title");
 const titleEN = "Khnami &mdash; Palliative care center";
 const titleRU = "Хнами &mdash; Центр паллиативной помощи";
@@ -18,14 +18,35 @@ const langSelectButtonsEN = document.querySelectorAll(".lang-select--en");
 const langSelectButtonsRU = document.querySelectorAll(".lang-select--ru");
 const langSelectButtonsARM = document.querySelectorAll(".lang-select--arm");
 
-//Always have an active language, which refers to according array with text elements
+// const patientName = document.querySelector("#patient_name");
+// const patientDiagnosis = document.querySelector("#patient_diagnosis");
+// const contactName = document.querySelector("#contact_name");
+// const contactEmail = document.querySelector("#contact_email");
+
+// Select inputs with text placeholders in the cta form
+const txtPlcInputs = document.querySelectorAll(".txtPlcInput");
+log(txtPlcInputs);
+
+// Creates an array of placeholder texts
+// patientName-patientDiagnosis-contactName-contactEmail
+const plcEN = ["Hakob Hakobyan", "Oncology", "Armine Hakobyan"];
+const plcRU = ["Акоп Акопян", "Онкология", "Армине Акопян"];
+const plcARM = ["Հակոբ Հակոբյան", "Ուռուցքաբանություն", "Արմինե Հակոբյան"];
+
+txtPlcInputs.forEach((el, i) => {});
+
+const translatePlaceholders = function () {};
+
+// Always have an active language, which refers to according array with text elements
 let activeTextElements = englishLanguage;
 
-//Translates the page to selected language
-function translatePage(newTextElements, newTitle) {
+// Translates the page to selected language
+const translatePage = function (newTextElements, newTitle) {
+  // Check if language is already active
   if (newTextElements == activeTextElements) {
     return;
   }
+  // Change visibility of elements
   activeTextElements.forEach(el => {
     el.classList.add("invisible");
   });
@@ -34,7 +55,7 @@ function translatePage(newTextElements, newTitle) {
   });
   activeTextElements = newTextElements;
   title.innerHTML = newTitle;
-}
+};
 
 //Translates to english
 langSelectButtonsEN.forEach(el => {
